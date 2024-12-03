c3352742146d6b18b690dd99201a0f65d13df90e
vsamopyt <v.samopyt14@gmail.com>
2024-12-03 17:31:22 +0100

    added dark-light-theme page

--- script/dark-light-theme.js
+++ script/dark-light-theme.js
@@ -1,33 +1,100 @@
-// BTN ONE
-const btnOne = document.querySelector('.btn-one-theme');
-const btnOneIcons = document.querySelectorAll('.btn-one-theme-icon');
+// thema
+const body = document.querySelector('body');
 
+
+// FUNCTIONS
+// TO CHANGE STYLE BTN
 const handleBtnStyle = function (element) {
- element.classList.toggle('darkBtn');
+  element.classList.toggle('darkBtn');
+  
 };
 
-const handleBtnIconStyle = (elements) => {
+//  TO CHANGE STYLE ICONS
+const handleBtnIconStyle = (elements, activeClass) => {
   elements.forEach(item => {
-    item.classList.toggle('schow');
+    item.classList.toggle(activeClass);
   });
 };
 
-const toggleBtnOne = function (element, elements) {
-    handleBtnStyle(element);
-    handleBtnIconStyle(elements)
+// TO CHECK THEME
+const checkTheme =(element, elements, activeClass) =>{
+    const localStorageTheme =localStorage.getItem("theme");
+    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    console.log(localStorageTheme);
+    console.log( systemPrefersDark);
+    
+    
+    if(localStorageTheme === "dark") {
+        element.classList.add("darkBtn")
+        elements.forEach(item => {
+            item.classList.toggle(activeClass);
+            body.classList.add("dark-theme")
+          });
+
+}};
+
+// save in localStorage
+const saveThemeToLocalStorage =() => {
+     const currentTheme = body.classList.contains("dark-theme") ? "dark" : "light";
+    localStorage.setItem("theme", currentTheme);
 }
 
+// TO SET THEME
+const handleThema = () => {
+    body.classList.toggle("dark-theme");
+}
 
-btnOne.addEventListener('click', (()=>{
-    toggleBtnOne(btnOne,btnOneIcons )
-}));
+// SET ALL FUNCTIONS
+const toggleBtnOne = function (element, elements, activeClass) {
+  handleThema(); 
+  handleBtnStyle(element);
+  handleBtnIconStyle(elements, activeClass);
+  saveThemeToLocalStorage()
+
+}
+
+// BTN ONE
+const btnOne = document.querySelector('.btn-one-theme');
+const btnOneIcons = document.querySelectorAll('.btn-one-theme-icon');
+checkTheme(btnOne, btnOneIcons, 'schow');
+btnOne.addEventListener('click', () => {
+  toggleBtnOne(btnOne, btnOneIcons, 'schow');
+});
+
+
 
 // BTN TWO
 
 const btnTwo = document.querySelector('.btn-two-theme');
 const btnTwoIcons = document.querySelectorAll('.btn-two-theme-icon');
-console.log(btnTwo,btnTwoIcons );
 
-btnTwo.addEventListener('click', (()=>{
-    toggleBtnOne(btnTwo, btnTwoIcons  )
-}));
+btnTwo.addEventListener('click', () => {
+  toggleBtnOne(btnTwo, btnTwoIcons, 'schow');
+});
+
+// BTN THREE
+
+const btnThree = document.querySelector('.btn-three-theme');
+const btnThreeIcons = document.querySelectorAll('.btn-three-icon-wrapper');
+console.log(btnTwo, btnTwoIcons);
+
+btnThree.addEventListener('click', () => {
+  toggleBtnOne(btnThree, btnThreeIcons, 'active');
+});
+
+// BTN FEAR
+
+const btnFear = document.querySelector('.btn-fear-theme');
+const btnFearIcons = document.querySelectorAll('.btn-fear-icon-wrapper');
+
+btnFear.addEventListener('click', () => {
+  toggleBtnOne(btnFear, btnFearIcons, 'active');
+});
+
+// BTN FIVE
+
+const btnFive = document.querySelector('.btn-five-theme');
+
+btnFive.addEventListener('click', () => {
+  toggleBtnOne(btnFive);
+});
